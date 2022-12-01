9d9f5525fa00b0bb7e07138cfdcd23ff4f5ca684
Robert Alexander <robbielalexander@gmail.com>
2022-12-01 21:42:22 +0000

    added spacey import statment

---
+++ garden.py
@@ -0,0 +1,16 @@
+# in this file I am going to analyse some garden path
+# sentences 
+
+import spacy 
+
+nlp = spacy.load('en_core_web_sm')
+
+garden_path_one = ""
+
+garden_path_two = ""
+
+garden_path_three = ""
+
+garden_path_four = ""
+
+garden_path_five = ""
